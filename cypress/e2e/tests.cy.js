990be1ecf1979421afd208a9f592e45cb5e01cc5
roman.kondrakov <roman.kondrakov@simbirsoft.ru>
2023-03-07 09:20:11 +0300

    fix

--- cypress/e2e/tests.cy.js
+++ cypress/e2e/tests.cy.js
@@ -1,26 +1,33 @@
-const nameUrl ='https://github.com/' 
-const name ='[title=SoftAssertions]'
-
 describe("Example test suite", () => {
   it(`Проверка наличия на странице текста "SoftAssertions"`, () => {
     cy.viewport(1920,1080)
-    cy.visit(nameUrl);
+    cy.visit("https://github.com/");
     cy.get('[aria-label="Search GitHub"]').type("selenide").type("{enter}");
     cy.get(".menu-item").contains("Wikis").click();
-    cy.get(name).should("be.visible").click();
+    cy.get("title=SoftAssertions").should("be.visible").click();
     cy.contains("SoftAssertions").should("be.visible");
-    cy.url().should('include','https://github.com/YuriKopshev/selenide/wiki/SoftAssertions');
-            
+    cy.url().should('include','https://github.com/YuriKopshev/selenide/wiki/SoftAssertions');     
 });
-it(`Проверка хедера habr.com на наличие заголовков "Хабр","Как стать автором","Неделя фронтенда"`, () => {
+
+it(`Проверка хедера habr.com на наличие заголовков "Хабр"`, () => {
   cy.viewport(1920,1080)
   cy.visit("https://habr.com/ru/all/");
   cy.get(".tm-header__logo-wrap").contains("Хабр").should("be.visible");
-  cy.get(".tm-header__container").contains("Как стать автором").should("be.visible");
-  cy.get(".tm-header__container").contains("Неделя фронтенда").should("be.visible");
+});   
 
-});      
-it("Наличие заголовков в меню хедера", () => {
+it(`Проверка хедера habr.com на наличие заголовков ""Как стать автором"`, () => {
+  cy.viewport(1920,1080)
+  cy.visit("https://habr.com/ru/all/");
+  cy.get(".tm-header__container").contains("Как стать автором").should("be.visible");
+});
+
+it(`Проверка хедера habr.com на наличие заголовков "Неделя фронтенда"`, () => {
+  cy.viewport(1920,1080)
+  cy.visit("https://habr.com/ru/all/");
+  cy.get(".tm-header__container").contains("Неделя фронтенда").should("be.visible");
+});
+
+it(`В меню хедера должен быть отображен заголовок "Все потоки","Разработка","Администрирование","Дизайн","Менеджмент","Маркетинг","Научпоп"`, () => {
   cy.viewport(1920,1080)
   cy.visit("https://habr.com/ru/all/");
   cy.get(".tm-base-layout__header").should("be.visible")
@@ -32,12 +39,14 @@ it("Наличие заголовков в меню хедера", () => {
   cy.get(".tm-base-layout__header").contains("Маркетинг").should("be.visible");
   cy.get(".tm-base-layout__header").contains("Научпоп").should("be.visible");
 });    
-it("Переход на страницу Разработка ", () => {
+
+it(`При нажатии на заголовок "Разработка" должен быть переход на страницу "Разработка"`, () => {
   cy.viewport(1920,1080)
   cy.visit("https://habr.com/ru/all/");
   cy.get('[href="/ru/flows/develop/"]').should("be.visible").click();
   cy.url().should('include','https://habr.com/ru/flows/develop/');
 }); 
+
 it(`клик по иконки выпадающего списка и выбор значения "Фриланс"`, () => {
   cy.viewport(1920,1080)
   cy.visit("https://habr.com/ru/all/");
