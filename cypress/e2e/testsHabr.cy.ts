535af1872ca8e693261753134375e589a412fdcc
roman.kondrakov <roman.kondrakov@simbirsoft.ru>
2023-03-09 17:00:43 +0300

    newFix

--- cypress/e2e/testsHabr.cy.ts
+++ cypress/e2e/testsHabr.cy.ts
@@ -2,11 +2,10 @@ import { MainPage } from "../pageobjects/MainPage";
 
 describe("Example test suite", () => {
   const mainPage = new MainPage();
-
-  const header = ".tm-header__container";
   const baseLayoutHeader = ".tm-base-layout__header";
   const iconDropDown = ".tm-header__icon_dropdown";
   const item = ".tm-our-projects__items";
+  const newUrl = "https://freelance.habr.com/";
 
   beforeEach(() => {
     cy.viewport(1920, 1080);
@@ -17,52 +16,42 @@ describe("Example test suite", () => {
     mainPage.visibleLogo();
   });
 
-  it(`Проверка хедера habr.com на наличие заголовков ""Как стать автором"`, () => {
-    cy.get(header).contains("Как стать автором").should("be.visible");
+  it(`Проверка хедера habr.com на наличие заголовков "Как стать автором"`, () => {
+    mainPage.textPresentHeaders("Как стать автором");
   });
-
   it(`Проверка хедера habr.com на наличие заголовков "Неделя фронтенда"`, () => {
-    cy.get(header).contains("Неделя фронтенда").should("be.visible");
+    mainPage.textPresentHeaders("Неделя фронтенда");
   });
 
   it(`В меню хедера должен быть отображен заголовок "Все потоки","Разработка","Администрирование","Дизайн","Менеджмент","Маркетинг","Научпоп"`, () => {
-    cy.get(baseLayoutHeader).should("be.visible");
-    cy.get(baseLayoutHeader).contains("Все потоки").should("be.visible");
-    cy.get(baseLayoutHeader).contains("Разработка").should("be.visible");
-    cy.get(baseLayoutHeader).contains("Администрирование").should("be.visible");
-    cy.get(baseLayoutHeader).contains("Дизайн").should("be.visible");
-    cy.get(baseLayoutHeader).contains("Менеджмент").should("be.visible");
-    cy.get(baseLayoutHeader).contains("Маркетинг").should("be.visible");
-    cy.get(baseLayoutHeader).contains("Научпоп").should("be.visible");
+    mainPage.textPresentHeadMenu("Все потоки");
+    mainPage.textPresentHeadMenu("Разработка");
+    mainPage.textPresentHeadMenu("Администрирование");
+    mainPage.textPresentHeadMenu("Дизайн");
+    mainPage.textPresentHeadMenu("Менеджмент");
+    mainPage.textPresentHeadMenu("Маркетинг");
+    mainPage.textPresentHeadMenu("Научпоп");
   });
 
   it(`При нажатии на заголовок "Разработка" должен быть переход на страницу "Разработка"`, () => {
-    cy.getHref('"/ru/flows/develop/"').should("be.visible").click();
-    cy.url().should("include", "https://habr.com/ru/flows/develop/");
+    mainPage.searchHrefAndClick('"/ru/flows/develop/"');
+    mainPage.checkUrl("https://habr.com/ru/flows/develop/")
+  
   });
 
   it(`клик по иконки выпадающего списка и выбор значения "Фриланс"`, () => {
-    cy.get(iconDropDown).click();
+    mainPage.searchAndClick(iconDropDown);
+    mainPage.elementPresents(item);
+    mainPage.searchHrefAndClick('"https://freelance.habr.com?utm_source=habr&utm_medium=habr_top_panel"')
     cy.get(item).should("be.visible");
-    cy.getHref(
-      '"https://freelance.habr.com?utm_source=habr&utm_medium=habr_top_panel"'
-    )
-      .should("be.visible")
-      .click();
+   
   });
 
   it('редирект на страницу "https://freelance.habr.com/"', () => {
-    cy.get(iconDropDown).click();
-    cy.getHref(
-      '"https://freelance.habr.com?utm_source=habr&utm_medium=habr_top_panel"'
-    )
-      .should("be.visible")
-      .click()
-      .first()
-      .then(($a) => {
-        const newUrl = "https://freelance.habr.com/";
+    mainPage.searchAndClick(iconDropDown);
+    mainPage.searchHrefAndClick('"https://freelance.habr.com?utm_source=habr&utm_medium=habr_top_panel"')
         cy.visit(newUrl);
         cy.getAriaLabel(`"Хабр Фриланс"`).should("be.visible");
       });
   });
-});
+
--- cypress/pageobjects/mainPage.js
+++ cypress/pageobjects/mainPage.js
@@ -1,3 +1,55 @@
 export class MainPage {
-  visibleLogo = () => cy.get('.tm-header__logo-wrap').contains("Хабр").should("be.visible");
+  logo = ".tm-header__logo-wrap";
+  headerСontainer = ".tm-header__container";
+  baseLayoutHeader = ".tm-base-layout__header";
+  /*
+   **метод проверки заголовка на главной странице
+   */
+  visibleLogo = () => cy.get(this.logo).contains("Хабр").should("be.visible");
+  /*
+   **метод проверки текста в шапке на главной странице
+   */
+  textPresentHeaders(text) {
+    cy.get(this.headerСontainer).contains(text).should("be.visible");
+  }
+  /*
+   **метод проверки текста в меню на главной странице
+   */
+  textPresentHeadMenu(text) {
+    cy.get(this.baseLayoutHeader).contains(text).should("be.visible");
+  }
+  /*
+   **метод поиска элемента по href и нажатие по нему
+   */
+  searchHrefAndClick(href) {
+    cy.getHref(href).should("be.visible").click();
+  }
+  /*
+   **метод проверки что URL соответствует ожидаемому
+   */
+  checkUrl(urls) {
+    cy.url().should("include", urls);
+  }
+  /*
+   **метод проверки что 'URL' соответствует ожидаемому
+   */
+   checkUrl(urls) {
+    cy.url().should("include", urls);
+  }
+ /*
+   **метод поиска элементy и нажатие по нему
+   */
+   searchAndClick(elements){
+   cy.get(elements).click();
 }
+
+/*
+   **метод проверки видимости элемента
+   */
+   elementPresents(element,text) {
+    cy.get(element).should("be.visible");
+  }
+
+
+  }
+  
