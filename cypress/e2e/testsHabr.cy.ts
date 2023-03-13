266e8b043bfa367cbf3581c970df4be8445567fc
roman.kondrakov <roman.kondrakov@simbirsoft.ru>
2023-03-13 10:06:57 +0300

    new fix

--- cypress/e2e/testsHabr.cy.ts
+++ cypress/e2e/testsHabr.cy.ts
@@ -15,14 +15,14 @@ describe("Example test suite", () => {
   });
 
   it(`Проверка хедера habr.com на наличие заголовков "Хабр"`, () => {
-    mainPage.elementPresents(logo,"Хабр");
+    mainPage.elementPresents(logo, "Хабр");
   });
 
   it(`Проверка хедера habr.com на наличие заголовков "Как стать автором"`, () => {
-    mainPage.elementPresents(headerСontainer,"Как стать автором");
+    mainPage.elementPresents(headerСontainer, "Как стать автором");
   });
   it(`Проверка хедера habr.com на наличие заголовков "Неделя фронтенда"`, () => {
-    mainPage.elementPresents(headerСontainer,"Неделя фронтенда");
+    mainPage.elementPresents(headerСontainer, "Неделя фронтенда");
   });
 
   it(`В меню хедера должен быть отображен заголовок "Все потоки","Разработка","Администрирование","Дизайн","Менеджмент","Маркетинг","Научпоп"`, () => {
@@ -37,23 +37,28 @@ describe("Example test suite", () => {
 
   it(`При нажатии на заголовок "Разработка" должен быть переход на страницу "Разработка"`, () => {
     mainPage.searchHrefAndClick('"/ru/flows/develop/"');
-    mainPage.checkUrl("https://habr.com/ru/flows/develop/")
-  
+    mainPage.checkUrl("https://habr.com/ru/flows/develop/");
   });
 
   it(`клик по иконки выпадающего списка и выбор значения "Фриланс"`, () => {
     mainPage.searchAndClick(iconDropDown);
     mainPage.elementPresents(item);
-    mainPage.searchHrefAndClick('"https://freelance.habr.com?utm_source=habr&utm_medium=habr_top_panel"')
+    mainPage.searchHrefAndClick(
+      '"https://freelance.habr.com?utm_source=habr&utm_medium=habr_top_panel"'
+    );
     cy.get(item).should("be.visible");
+  });
+
+  it.only('редирект на страницу "https://freelance.habr.com/"', () => {
    
-  });
-
-  it('редирект на страницу "https://freelance.habr.com/"', () => {
     mainPage.searchAndClick(iconDropDown);
-    mainPage.searchHrefAndClick('"https://freelance.habr.com?utm_source=habr&utm_medium=habr_top_panel"')
-        cy.visit(newUrl);
-        cy.getAriaLabel(`"Хабр Фриланс"`).should("be.visible");
-      });
+    
+    mainPage.searchHrefAndClick(
+      '"https://freelance.habr.com?utm_source=habr&utm_medium=habr_top_panel"'
+    );
+    cy.visit(newUrl);
+    cy.intercept("GET", "https://freelance.habr.com/");
+    cy.getAriaLabel(`"Хабр Фриланс"`).should("be.visible");
+    cy.intercept("");
   });
-
+});
